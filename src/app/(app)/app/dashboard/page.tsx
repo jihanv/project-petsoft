c03e93755f7dc90ea6855e962a132f82be69f98a
jihanv <hedjmny@gmail.com>
2025-08-24 12:33:21 +0900

    Fetch Petlist data

--- src/app/(app)/app/dashboard/page.tsx
+++ src/app/(app)/app/dashboard/page.tsx
@@ -6,7 +6,17 @@ import PetList from "@/components/pet-list";
 import SearchForm from "@/components/search-form";
 import Stats from "@/components/stats";
 
-export default function Home() {
+export default async function Home() {
+
+    const response = await fetch("https://bytegrad.com/course-assets/projects/petsoft/api/pets")
+
+    //Check if everything is ok with the data fetch
+    if (!response.ok) {
+        throw new Error("Could not fetch pets")
+    }
+
+    const data = await response.json()
+
     return (
         <main>
             <div className="flex items-center justify-between text-white py-8">
@@ -20,7 +30,7 @@ export default function Home() {
                 </div>
                 <div className="md:row-start-2 md:row-span-full md:col-start-1 md:col-span-1">
                     <ContentBlock>
-                        <PetList />
+                        <PetList pets={data} />
                     </ContentBlock>
                 </div>
                 <div className="md:row-start-1 md:row-span-full md:col-start-2 md:col-span-full">
--- src/components/pet-list.tsx
+++ src/components/pet-list.tsx
@@ -1,9 +1,21 @@
-export default function PetList() {
+import Image from "next/image";
+
+export default function PetList({ pets }) {
     return (
         <ul className="bg-white border-b border-black/[0.08]">
-            <li>
-                <button>Benjamin</button>
-            </li>
+
+            {pets.map((pet) => (
+                <li key={pet.id}>
+                    <button className="flex h-[70px] w-full cursor-pointer items-center px-5 text-base gap-3 hover:bg-[#EFF1F2] focus:bg-[#EFF1F2] transition">
+                        <Image src={pet.imageUrl}
+                            alt="Pet image"
+                            width={45}
+                            height={45}
+                            className="rounded-full object-cover w-[45px] h-[45px]" />
+                        <p className="font-semibold">{pet.name}</p>
+                    </button>
+                </li>))}
+
         </ul>
     );
 }
