608f2ed5640a9aeae695275c04c913f239e3080f
jihanv <hedjmny@gmail.com>
2025-08-24 09:38:23 +0900

    Create branding and stats component for dashboard

--- src/app/(app)/app/dashboard/page.tsx
+++ src/app/(app)/app/dashboard/page.tsx
@@ -1,4 +1,14 @@
+import Branding from "@/components/branding";
+import H1 from "@/components/h1"
+import Stats from "@/components/stats";
 
 export default function Home() {
-    return <main>Dashboard</main>;
+    return (
+        <main>
+            <div className="flex items-center justify-between text-white py-8">
+                <Branding />
+                <Stats />
+            </div>
+        </main >
+    );
 }
