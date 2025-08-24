97c665b35142f450ae67b14879d58e90bec39242
jihanv <hedjmny@gmail.com>
2025-08-24 09:24:28 +0900

    Implement private space layout footer

--- src/components/app-footer.tsx
+++ src/components/app-footer.tsx
@@ -1,6 +1,10 @@
 
 export default function AppFooter() {
     return (
-        <div>app-footer</div>
+        <footer className="border-t border-black/5 py-5 mt-auto">
+            <small className="opacity-50">
+                &copy; 2030 Bytgrad. All rights reserved.
+            </small>
+        </footer>
     )
 }
