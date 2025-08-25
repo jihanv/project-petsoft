a276f76ac211746163c6be3ef5e990b4b17c960d
jihanv <hedjmny@gmail.com>
2025-08-25 21:55:04 +0900

    Add content to the account page

--- src/app/(app)/app/account/page.tsx
+++ src/app/(app)/app/account/page.tsx
@@ -1,4 +1,12 @@
+import ContentBlock from "@/components/content-block";
+import H1 from "@/components/h1";
 
 export default function Home() {
-    return <main>Account</main>;
+    return (<main>
+        <H1 className=" text-white my-8">Your Account</H1>
+
+        <ContentBlock className="h-[500px] flex justify-center items-center">
+            <p>Logged in as ...</p>
+        </ContentBlock>
+    </main>);
 }
