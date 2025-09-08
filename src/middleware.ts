c344c1e4743d9d8abfb2e563d481073a1b47dd09
jihanv <hedjmny@gmail.com>
2025-09-08 18:50:35 +0900

    Add middleware basic route.

---
+++ src/middleware.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+
+export function middleware(request: Request) {
+  //Get url the user is using.
+  request.url;
+
+  console.log(request.url);
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+  ],
+};
