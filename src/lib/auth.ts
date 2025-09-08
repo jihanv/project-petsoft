63d71d0f210f3e742a4e2847094d462dbb278f15
jihanv <hedjmny@gmail.com>
2025-09-08 20:52:33 +0900

    Add logic to authorization

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -5,7 +5,16 @@ const config = {
     signIn: "/login",
   },
   providers: [],
-  callbacks: {},
+  callbacks: {
+    authorized: ({ request }) => {
+      const isTryingToAccessApp = request.nextUrl.pathname.includes("/app");
+      if (isTryingToAccessApp) {
+        return false;
+      } else {
+        return true;
+      }
+    },
+  },
 } satisfies NextAuthConfig;
 
 export const { auth } = NextAuth(config);
