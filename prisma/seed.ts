02fbd083bf1d73dffa7c84abd0190ebf6eb73e2c
jihanv <hedjmny@gmail.com>
2025-09-08 19:29:05 +0900

    Seed database with user

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,41 +1,47 @@
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
+import bcrypt from "bcryptjs";
 
 const prisma = new PrismaClient();
 
-const pets = [
-  {
-    name: "Ben",
-    ownerName: "John Doe",
-    imageUrl: "/rn-image-4.png",
-    age: 2,
-    notes:
-      "Doesn't like to be touched on the belly. Plays well with other dogs.",
+const userData: Prisma.UserCreateInput = {
+  email: "example@gmail.com",
+  hashedPassword: "",
+  pets: {
+    create: [
+      {
+        name: "Benjamin",
+        ownerName: "John Doe",
+        imageUrl: "/rn-image-4.png",
+        age: 2,
+        notes:
+          "Doesn't like to be touched on the belly. Plays well with other dogs.",
+      },
+      {
+        name: "Richard",
+        ownerName: "Josephine Dane",
+        imageUrl: "/rn-image-5.png",
+        age: 5,
+        notes: "Needs medication twice a day.",
+      },
+      {
+        name: "Anna",
+        ownerName: "Frank Doe",
+        imageUrl: "/rn-image-6.png",
+        age: 4,
+        notes: "Allergic to chicken.",
+      },
+    ],
   },
-  {
-    name: "Richard",
-    ownerName: "Josephine Dane",
-    imageUrl: "/rn-image-5.png",
-    age: 5,
-    notes: "Needs medication twice a day.",
-  },
-  {
-    name: "Anna",
-    ownerName: "Frank Doe",
-    imageUrl: "/rn-image-6.png",
-    age: 4,
-    notes: "Allergic to chicken.",
-  },
-];
+};
 
 async function main() {
   console.log(`Start seeding ...`);
 
-  for (const pet of pets) {
-    const result = await prisma.pet.create({
-      data: pet,
-    });
-    console.log(`Created pet with id: ${result.id}`);
-  }
+  const hashedPassword = await bcrypt.hash("example", 10);
+
+  await prisma.user.create({
+    data: userData,
+  });
 
   console.log(`Seeding finished.`);
 }
@@ -49,6 +55,3 @@ main()
     await prisma.$disconnect();
     process.exit(1);
   });
-
-//seed
-//npx prisma db seed
