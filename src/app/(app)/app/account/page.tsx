a44734f437f54ac6e328be671f6087982f259080
jihanv <hedjmny@gmail.com>
2025-09-10 19:35:04 +0900

    Render user email in the UI

--- src/app/(app)/app/account/page.tsx
+++ src/app/(app)/app/account/page.tsx
@@ -1,12 +1,17 @@
 import ContentBlock from "@/components/content-block";
 import H1 from "@/components/h1";
+import { auth } from "@/lib/auth"
 
-export default function Home() {
+export default async function Home() {
+
+    const session = await auth()
     return (<main>
         <H1 className=" text-white my-8">Your Account</H1>
 
         <ContentBlock className="h-[500px] flex justify-center items-center">
-            <p>Logged in as ...</p>
+            <p>Logged in as {session?.user?.email}</p>
         </ContentBlock>
     </main>);
 }
+// use auth() to access logged user email for the session (server)
+// use useSession() to access logged user email for the session (client)
