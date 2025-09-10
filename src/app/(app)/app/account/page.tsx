60cb25175d534b76812b9005904938c8dcf3dd4f
jihanv <hedjmny@gmail.com>
2025-09-10 20:39:32 +0900

    Set up Sign Out Button

--- src/app/(app)/app/account/page.tsx
+++ src/app/(app)/app/account/page.tsx
@@ -1,15 +1,23 @@
 import ContentBlock from "@/components/content-block";
 import H1 from "@/components/h1";
+import SignOutBtn from "@/components/sign-out-btn";
+import { Button } from "@/components/ui/button";
 import { auth } from "@/lib/auth"
+import { redirect } from "next/navigation";
 
 export default async function Home() {
 
     const session = await auth()
+    if (!session?.user) {
+        redirect("/login")
+    }
     return (<main>
         <H1 className=" text-white my-8">Your Account</H1>
 
-        <ContentBlock className="h-[500px] flex justify-center items-center">
-            <p>Logged in as {session?.user?.email}</p>
+        <ContentBlock className="h-[500px] flex flex-col gap-3 justify-center items-center">
+            <p>Logged in as {session.user.email}</p>
+
+            <SignOutBtn />
         </ContentBlock>
     </main>);
 }
---
+++ src/components/sign-out-btn.tsx
@@ -0,0 +1,8 @@
+"use client"
+import { Button } from "./ui/button";
+
+export default function SignOutBtn() {
+    return (
+        <Button onClick={() => { }}>Sign Out</Button>
+    )
+}
