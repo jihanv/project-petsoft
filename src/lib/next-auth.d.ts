bfeb87a9ba981d7cfcb455547a38e16443ae67c4
jihanv <hedjmny@gmail.com>
2025-09-13 20:13:12 +0900

    Change session NextAuth type

--- src/lib/next-auth.d.ts
+++ src/lib/next-auth.d.ts
@@ -1,4 +1,4 @@
-import type { DefaultSession } from "next-auth";
+import type { DefaultSession, User } from "next-auth";
 
 // import { DefaultSession } from "next-auth";
 // declare module "next-auth" {
@@ -20,5 +20,13 @@ import type { DefaultSession } from "next-auth";
 declare module "next-auth/jwt" {
   interface JWT {
     userId?: string;
+  }
+}
+
+declare module "next-auth" {
+  interface Session {
+    user: User & {
+      id: string;
+    };
   }
 }
