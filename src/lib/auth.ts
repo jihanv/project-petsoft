7fba0a079a7388f1fc335d5b1dcff1c6aced58f0
jihanv <hedjmny@gmail.com>
2025-09-18 21:15:39 +0900

    388 Change redirect rules

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -52,18 +52,22 @@ const config = {
       if (!isLoggedIn && isTryingToAccessApp) {
         return false;
       }
+      if (isLoggedIn && isTryingToAccessApp && !auth?.user.hasAccess) {
+        return Response.redirect(new URL("/payment", request.nextUrl));
+      }
+
       if (isLoggedIn && isTryingToAccessApp && auth?.user.hasAccess) {
         return true;
       }
 
       if (isLoggedIn && !isTryingToAccessApp) {
         if (
-          request.nextUrl.pathname.includes("/login") ||
-          request.nextUrl.pathname.includes("/signup")
+          (request.nextUrl.pathname.includes("/login") ||
+            request.nextUrl.pathname.includes("/signup")) &&
+          !auth?.user.hasAccess
         ) {
           return Response.redirect(new URL("/payment", request.nextUrl));
         }
-
         return true;
       }
 
