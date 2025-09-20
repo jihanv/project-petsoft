107d2751504eeedc4a68589c9aef8a381ce07d4e
jihanv <hedjmny@gmail.com>
2025-09-20 21:41:48 +0900

    390 Update database upon successful payment

--- src/app/api/stripe/route.ts
+++ src/app/api/stripe/route.ts
@@ -3,24 +3,25 @@ import { prisma } from "@/lib/db";
 export async function POST(request: Request) {
   // Stripe will send a webhook with json data
 
-  const data = await request.text();
-
-  console.log(data);
+  const data = await request.json();
 
   // Verify webhook came from stripe
 
   // Fufill order
-  //   await prisma.user.update({
-  //     where: {
-  //       email: data.data.object.customer_email,
-  //     },
-  //     data: {
-  //       hasAccess: true,
-  //     },
-  //   });
+  await prisma.user.update({
+    where: {
+      email: data.data.object.customer_email,
+    },
+    data: {
+      hasAccess: true,
+    },
+  });
 
-  //   // Return 200 OK
+  console.log("Approved!" + data.data.object.customer_email);
+
+  // Return 200 OK
   return Response.json(null, { status: 200 });
 }
 
-//Open stripe then open with cmd.exe
+//Open folder that has stripe then open with cmd.exe
+// stripe listen --forward-to localhost:3000/api/stripe
