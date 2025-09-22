bfbe7b6a2eb21dfa416001afc11c3ca8fc98d07d
jihanv <hedjmny@gmail.com>
2025-09-22 19:54:29 +0900

    393 Verify webhook

--- src/app/api/stripe/route.ts
+++ src/app/api/stripe/route.ts
@@ -1,23 +1,47 @@
 import { prisma } from "@/lib/db";
 
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
 export async function POST(request: Request) {
   // Stripe will send a webhook with json data
 
-  const data = await request.json();
+  // const data = await request.json();
 
+  const body = await request.text();
+  const signature = request.headers.get("stripe-signature");
   // Verify webhook came from stripe
 
-  // Fufill order
-  await prisma.user.update({
-    where: {
-      email: data.data.object.customer_email,
-    },
-    data: {
-      hasAccess: true,
-    },
-  });
+  let event;
 
-  console.log("Approved!" + data.data.object.customer_email);
+  try {
+    event = stripe.webhooks.constructEvent(
+      body,
+      signature,
+      process.env.STRIPE_WEBHOOK_SECRET
+    );
+  } catch (error) {
+    console.log("Webhook verification failed.", error);
+    return Response.json(null, { status: 400 });
+  }
+
+  // Fufill order
+
+  switch (event.type) {
+    case "checkout.session.completed":
+      await prisma.user.update({
+        where: {
+          email: event.data.object.customer_email,
+        },
+        data: {
+          hasAccess: true,
+        },
+      });
+      console.log("Approved!" + event.data.object.customer_email);
+
+      break;
+    default:
+      console.log("Unhandled event type ", event.type);
+  }
 
   // Return 200 OK
   return Response.json(null, { status: 200 });
